7ad8e036f6e8c11112c83b54b47be62f40e84ea8
Chirag Jain <52311416+sudo-coder9@users.noreply.github.com>
2021-07-28 18:21:52 +0530

    Update settings.py

--- AiObj/AiObj/settings.py
+++ AiObj/AiObj/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5%tx!0+ym@hf8i2k35m#_5mf0q6kx-u*%pxpfxb&mf(h&1_*vo'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
